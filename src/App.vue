635257ef4857697e570d67f15e34824a5057a9ea
2Coo85 <63703656+2Coo85@users.noreply.github.com>
2021-06-09 09:17:27 -0500

    initial commitment to database

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container-fluid">
+      <router-view/>
+      <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+    name: '#app',
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style lang="scss">
+    html {
+        background-image: url('./img/waterfall-cave.jpg');
+    }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="nav">
+        <nav class="main-nav">
+            <ul>
+                <li><a href="P3_02_webpage.html">Home</a></li>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="contact.html">Resume</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
